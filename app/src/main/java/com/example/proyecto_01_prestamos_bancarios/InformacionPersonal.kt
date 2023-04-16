58d3d0f3a00db4e338efb7b10eb3c0771d2e848d
Gabriel Barboza Carvajal <gabriel.barboza.carvajal@est.una.ac.cr>
2023-04-16 16:58:42 -0600

    Pantalla informacion personal terminada correctamente
    se termino la pantalla 9, solo falta agregar el campo para el telefono !!

--- app/src/main/java/com/example/proyecto_01_prestamos_bancarios/InformacionPersonal.kt
+++ app/src/main/java/com/example/proyecto_01_prestamos_bancarios/InformacionPersonal.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -29,7 +31,7 @@ class InformacionPersonal : AppCompatActivity() {
     private lateinit var radioButtonDivorciado:RadioButton
     private lateinit var radioButtonViudo:RadioButton
     private lateinit var radioButtonGrupoEstadoCivil:RadioGroup
-
+    private lateinit var estado_civil:String
 
 
     private lateinit var editTexDireccion:EditText
@@ -39,7 +41,9 @@ class InformacionPersonal : AppCompatActivity() {
         setContentView(R.layout.activity_informacion_personal)
 
         asignandoVariablesVista()
+        btnModificar.setOnClickListener { modificarDatos() }
         cargandoVariablesVista()
+
     }
 
     fun cargandoVariablesVista(){
@@ -71,14 +75,19 @@ class InformacionPersonal : AppCompatActivity() {
 
                         if(estadoCivil=="Soltero") {
                             radioButtonSoltero.isChecked = true
+                            estado_civil="Soltero"
                         }else if(estadoCivil=="Divorciado"){
                             radioButtonDivorciado.isChecked=true
+                            estado_civil="Divorciado"
                         }else if(estadoCivil=="Viudo"){
                             radioButtonViudo.isChecked=true
+                            estado_civil="Viudo"
                         }else if(estadoCivil=="Casado"){
                             radioButtonCasado.isChecked=true
+                            estado_civil="Casado"
                         }else{
                             radioButtonGrupoEstadoCivil.clearCheck()
+                            estado_civil=""
                         }
 
                         editTextNombre.setText(nombre)
@@ -117,20 +126,38 @@ class InformacionPersonal : AppCompatActivity() {
     }
 
     fun modificarDatos(){
-        // al presionar el botón modificar
-        btnModificar.setOnClickListener{
-            val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("clientes")
-            //obtener el usuario logueado
-            // Obtener referencia a SharedPreferences
-            val prefs = getSharedPreferences("infoUsuario", Context.MODE_PRIVATE)
+        //obtener el usuario logueado
+        // Obtener referencia a SharedPreferences
+        val prefs = getSharedPreferences("infoUsuario", Context.MODE_PRIVATE)
 
-            val hashUsuario = prefs.getString("hasUsuario", "0")
+        val hashUsuario = prefs.getString("hasUsuario", "0")
+
+
+            val database = FirebaseDatabase.getInstance()
+            val myRef = hashUsuario?.let { it1 -> database.getReference("clientes").child(it1) }
+
+        val selectedRadioButtonId = radioButtonGrupoEstadoCivil.checkedRadioButtonId
+        var esta_strin = estado_civil
+        if (selectedRadioButtonId != -1) {
+            val selectedRadioButton = findViewById<RadioButton>(selectedRadioButtonId)
+            val radioButtonValue = selectedRadioButton.text.toString()
+            esta_strin=radioButtonValue
+        }
+
+        val nuevosValores = mapOf<String, Any>(
+                "nombre" to editTextNombre.text.toString(),
+                "salario" to editTextSalario.text.toString(),
+                "fecha_nacimiento" to editTextFechaNacimiento.text.toString(),
+                "estado_civil" to esta_strin,
+                "direccion" to editTexDireccion.text.toString()
+            )
             if (hashUsuario != null) {
-                myRef.
+                if (myRef != null) {
+                    myRef.updateChildren(nuevosValores)
+                }
             }
 
-        }
+        Toast.makeText(this, "Datos actualizados!", Toast.LENGTH_SHORT).show()
     }
 
     override fun onBackPressed() {
